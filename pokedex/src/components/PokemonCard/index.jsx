20edb1823a5319318aaa43576a95146e00cd5ba8
Davi <davijustinocouto@gmail.com>
2022-12-14 11:05:12 -0300

    a

--- pokedex/src/components/PokemonCard/index.jsx
+++ pokedex/src/components/PokemonCard/index.jsx
@@ -1,23 +1,21 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import {Button, CardActionsArea, CardActions} from "@mui/material";
 
-export default function PokemonCard() {
+export default function PokemonCard({name, image}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
+        image={image}
         alt="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        {name}
         </Typography>
         {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
--- pokedex/src/pages/Home.jsx
+++ pokedex/src/pages/Home.jsx
@@ -1,41 +1,37 @@
 import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import PokemonCard from "../components/PokemonCard";
-import axios from "axios";
+// import {Skeletons} from "../components/Skeletons"
 
 export const Home = () => {
-  useEffect(()=>{
+  const [pokemons, setPokemons] = useState([])
+    useEffect(()=>{
     getPokemon();
-  });
+  }, []);
   const getPokemon = () => {
-    axios
-    .get("https://pokeapi.co/api/v2/pokemon?limit=50")
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-  }
+    var endpoints = []
+    for(var i = 1; i < 200; i++ ){
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    }
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));    // axios
+    // .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+    // .then((res) => setPokemons(res.data))
+    // .catch((err) => console.log(err));
+  };
 
   return (
     <div>
       <Navbar />
       <Container maxWidth="false">
-        <Grid container>
-          <Grid item xs={3}>
-            <PokemonCard />
+        <Grid container spacing={3}>
+          {pokemons.map((pokemon, key) => (
+          <Grid item xs={2} key={key}>
+                <PokemonCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} types={pokemon.data.types} />
             </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-            </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-            </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-            </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-            </Grid>
+            ))}
         </Grid>
       </Container>
     </div>
